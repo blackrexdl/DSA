dae637b4db987811c23c0951f26b61c113f7ebca
Shubham Sharma <sharmashibu787@gmail.com>
2026-02-14 18:56:38 +0530

    `Added newline at end of file and added comment explaining the code`

--- step1.cpp
+++ step1.cpp
@@ -4,3 +4,4 @@ int main(){
   cout<<"Shubham"<<" Sharma" <<" Hello World!"<<endl;
   return 0;
 }
+// This code will print "Shubham Sharma Hello World!" to the console. The '<<' operator is used to concatenate the strings and the 'endl' is used to insert a newline character at the end of the output.
