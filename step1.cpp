2181cc70bba75ac0f7d8871609bc65dc9968c76e
Shubham Sharma <sharmashibu787@gmail.com>
2026-02-14 20:21:25 +0530

    char grade ='A';

--- step1.cpp
+++ step1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-// sizeof operator is used to find the size of a data type or variable in bytes
+// char grade ='A';
  int  age =25;
-cout<<"My age is "<<age<<" and its size is "<<sizeof(age)<<" bytes"<<endl;
-
+ char grade ='A';
+cout<<"Size of char: " <<grade <<" bytes"<<endl;
   return 0;
 }
