adaedc2522d3ec9fd408c176fd3d482cf328418e
Shubham Sharma <sharmashibu787@gmail.com>
2026-02-14 19:06:07 +0530

    Variables and Data Types

--- step1.cpp
+++ step1.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-  cout<<"Shubham"<<" Sharma" <<" Hello World!"<<endl;
+// Variables and Data Types
+ int  age =25;
+cout<<"My age is "<<age<<endl;
+
   return 0;
 }
-// This code will print "Shubham Sharma Hello World!" to the console. The '<<' operator is used to concatenate the strings and the 'endl' is used to insert a newline character at the end of the output.
