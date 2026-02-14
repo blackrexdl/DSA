a6746ef408b527d2c71b4371e06927c5c671e933
Shubham Sharma <sharmashibu787@gmail.com>
2026-02-14 20:16:29 +0530

    sizeof operator

--- step1.cpp
+++ step1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-// Variables and Data Types
+// sizeof operator is used to find the size of a data type or variable in bytes
  int  age =25;
-cout<<"My age is "<<age<<endl;
+cout<<"My age is "<<age<<" and its size is "<<sizeof(age)<<" bytes"<<endl;
 
   return 0;
 }
