5e353afad2a3db9190cdbb98f62aba4c7524dadb
Shubham Sharma <sharmashibu787@gmail.com>
2026-02-18 17:47:34 +0530

    conditonal statment's

--- step1.cpp
+++ step1.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-  
- cout <<5/ (double)2 << endl;// 5/2 will give 2 but 5/(double)2 will give 2.5 because of type casting
-  return 0;
+  // This program checks if the user is an adult or a minor based on their age.conditional statements
+int age;
+cout<<"Enter your age: ";
+cin>>age;
+if(age>=18){
+    cout<<"You are an adult."<<endl;
+}
+else{
+    cout<<"You are a minor."<<endl;
+}
 }
